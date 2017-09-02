2aea691aefdd0bd30fb5d8cd87e8c845c4f5b8b2
Alan Murad <alan.murad42@gmail.com>
2017-09-02 16:00:40 +0100

    4.5 add carousel to landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def index
   end
   def landing_page
-    @featured_product = Product.first
+    @featured_products = Product.first
+    @products = Product.limit(3)
   end
 end
