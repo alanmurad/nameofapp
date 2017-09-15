a7f690b70eeb0217c2bd9664014bdbce7d8f5656
Alan Murad <alan.murad42@gmail.com>
2017-09-16 00:52:02 +0100

    4.1

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :products
   get 'static_pages/about'
-
   get 'static_pages/contact'
-
   get 'static_pages/index'
-  
-  root 'static_pages#landing_page'
-
-
+  get 'static_pages/landing_page'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
