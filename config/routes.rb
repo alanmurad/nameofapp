06c3fd2809ffbdb32c4ae4b6d79081d4562231d9
Alan Murad <alan.murad42@gmail.com>
2017-09-16 02:22:24 +0100

    5.2 add search form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   get 'static_pages/index'
-  get 'static_pages/landing_page'
-  root 'products#index'
+  root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
