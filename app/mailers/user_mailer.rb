653ef69d272ac21b277569549fe44f49d5e4c797
Alan Murad <alan.murad42@gmail.com>
2017-09-25 13:49:29 +0100

    Fixing authentication issues

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,17 @@
 class UserMailer < ApplicationMailer
-	default from: 'alan.murad@protonmail.com'
+	default from: 'alan.murad42@gmail.com'
 	
 	def contact_form(email, name, message)
   	@message = message
     mail(from: email,
       to: 'alan.murad42@gmail.com',
       subject: "A new contact form message from #{name}")
+  end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
