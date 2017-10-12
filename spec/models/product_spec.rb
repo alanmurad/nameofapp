3bf344fd523f0d6c6df11d96317e39f5a4f7b3d2
Alan Murad <alan.murad42@gmail.com>
2017-10-12 20:00:55 +0100

    6.2 Unit Test & RSpec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,22 +2,18 @@ require 'rails_helper'
 
 describe Product do
 	context "when the product has comments" do	
-		let(:product) { Product.create!(name: "race bike", description: "Very fast bike", price: "99", colour: "green", image_url: "carouselbikeone.jpg") }
-		let(:user) { User.create!(first_name: "Alan", last_name:"Murad", email:"myemail@mydomain.com", password: "12345")}
 		before do
-	  	product.comments.create!(rating: 1, user: user, body: "Awful bike!")
-	  	product.comments.create!(rating: 3, user: user, body: "Ok bike!")
-	  	product.comments.create!(rating: 5, user: user, body: "Great bike!")
+			@product = Product.create!(name: "race bike", description: "Very fast bike", price: "99", colour: "green", image_url: "carouselbikeone.jpg") 
+			@user = User.create!(first_name: "Alan", last_name:"Murad", email:"myemail@mydomain.com", password: "123456")
+	  	@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+	  	@product.comments.create!(rating: 3, user: @user, body: "Ok bike!")
+	  	@product.comments.create!(rating: 5, user: @user, body: "Great bike!")
 		end
 		it "returns the average rating of all comments" do
-			expect(product.average_rating).to eq 3 
+			expect(@product.average_rating).to eq(3)
+		end
+		it "is not valid without a name" do 
+			expect(Product.new(description: "Nice bike")).not_to be_valid			
 		end
 	end
-	context "when the product has no name" do
-		let(:product) { Product.new(description: "Nice bike") }
-		it "should not be valid" do 
-			expect(Product.new(description: "Nice bike")).not_to_be_valid		
-		end		
-	end
-
 end
