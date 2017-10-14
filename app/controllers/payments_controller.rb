52909f241940c16f9de26871a4815f5c950b81a5
Alan Murad <alan.murad42@gmail.com>
2017-10-14 05:37:32 +0100

    6.6 creating Swipe payments

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,21 @@
+class PaymentsController < ApplicationController
+	def create
+		@product = Product.find(params[:product_id])
+		@user = current_user
+	  token = params[:stripeToken]
+	  # Create the charge on Stripe's servers - this will charge the user's card
+	  
+	  begin
+	    charge = Stripe::Charge.create(
+	      amount: (@product.price*100).to_i,
+	      currency: "gbp",
+	      source: token,
+	      description: params[:stripeEmail]
+	    )
+	  rescue Stripe::CardError => e
+	    # The card has been declined
+	  end
+
+	  redirect_to product_path(@product)
+	end
+end
