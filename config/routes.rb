bf132441cd24f7aa54c1c1a9764fcd8649257940
alanmurad <alan.murad42@gmail.com>
2017-12-14 03:23:17 +0000

    6.6 fixing payments

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   get 'static_pages/landing_page'
   post 'static_pages/thank_you'
   post 'payments/create'
-  get 'payments/create'
 
   root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
