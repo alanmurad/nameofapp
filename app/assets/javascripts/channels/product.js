84b923e0d0585ab094b6a2da8f31daac07c438da
alanmurad <alan.murad42@gmail.com>
2018-01-04 12:41:48 +0000

    6.14 Advanced ActionCable, display comments

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -7,9 +7,13 @@ App.product = App.cable.subscriptions.create("ProductChannel",
     // Called when the subscription has been terminated by the server
   },
 
-  received: function() {
+  received: function(data) {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
+    $(".product-reviews").prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
+    // console.log(data.comment.body);
   },
 
   listen_to_comments: function() {
@@ -17,8 +21,8 @@ App.product = App.cable.subscriptions.create("ProductChannel",
   		product_id: $("[data-product-id]").data("product-id")
   	});
   }
+});
 
-  $(document).on('turbolinks:load', function() {
-  	App.product.listen_to_comments();
-  });
+$(document).on('turbolinks:load', function() {
+ 	App.product.listen_to_comments();
 });
